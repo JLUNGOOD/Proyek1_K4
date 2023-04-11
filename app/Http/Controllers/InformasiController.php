3ce04053b528acb880eeef3c92ecb3f24c28e756
Virgy Ferdian Surya Firmansyah <virgyferdian213@gmail.com>
2023-04-11 10:36:05 +0700

    add migration & controller for model informasi

---
+++ app/Http/Controllers/InformasiController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\InformasiModel;
+use Illuminate\Http\Request;
+
+class InformasiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\InformasiModel  $informasi
+     * @return \Illuminate\Http\Response
+     */
+    public function show(InformasiModel $informasi)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\InformasiModel  $informasi
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(InformasiModel $informasi)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\InformasiModel  $informasi
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, InformasiModel $informasi)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\InformasiModel  $informasi
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(InformasiModel $informasi)
+    {
+        //
+    }
+}
