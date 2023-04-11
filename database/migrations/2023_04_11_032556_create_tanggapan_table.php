ae8bb0111d6f906aded93525e8c209f249a1213c
Bahtiar Rifai <bahtiarderifai@gmail.com>
2023-04-11 10:44:15 +0700

    add migration $ controller for tanggapan

---
+++ database/migrations/2023_04_11_032556_create_tanggapan_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tanggapan', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('pengaduan_id')->constrained('pengaduan');
+            $table->foreignId('user_id')->constrained('users');
+            $table->text('isi_tanggapan');
+            $table->boolean('is_read')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tanggapan');
+    }
+};
