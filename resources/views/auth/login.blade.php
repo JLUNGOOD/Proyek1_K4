27d78dc75224fe07b9fff02be205bff9bdb11a2e
Bahtiar Rifai <bahtiarderifai@gmail.com>
2023-05-02 09:11:38 +0700

    move file login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,117 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+</head>
+<body>
+<div class="container col-xl-10 col-xxl-8 px-4">
+    <div class="row align-items-center g-lg-5 h-100vh">
+        <div class="col-lg-7">
+            <img src="{{ asset('img/undraw_Login_re_4vu2.png') }}" class="d-block mx-lg-auto img-fluid" alt="Login"
+                 loading="lazy">
+        </div>
+        <div class="col-md-10 mx-auto col-lg-5">
+            <form class="p-4 p-md-5 border rounded-3 bg-light shadow" method="post">
+                <h1 class="display-5 fw-bold mb-4 text-center">Login</h1>
+                <div class="form-floating mb-3">
+                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"
+                           name="email">
+                    <label for="floatingInput">Email Address</label>
                 </div>
-            </div>
+                <div class="form-floating mb-3">
+                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password"
+                           name="password">
+                    <label for="floatingPassword">Password</label>
+                </div>
+                <div class="checkbox mb-3">
+                    <label>
+                        <input type="checkbox" class="form-check-input" value="remember-me" name="remember"> Ingat saya
+                    </label>
+                </div>
+                <button class="w-100 btn btn-lg btn-dark" type="submit" name="masuk">Sign In</button>
+                <hr class="my-4">
+                <small class="d-block text-muted text-center">Belum punya akun? <a href="" data-bs-toggle="modal"
+                                                                                   data-bs-target="#daftar">Daftar</a>.
+                </small>
+            </form>
         </div>
     </div>
 </div>
-@endsection
+
+<div class="modal fade" id="daftar" data-bs-backdrop="static" tabindex="-1" aria-labelledby="daftar"
+     aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fw-bold mb-0 fs-2" id="exampleModalLabel">Daftar</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form method="post">
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <label for="firstname" class="form-label">Nama Depan</label>
+                            <input class="form-control" name="firstname" id="firstname" placeholder="Nama Depan *"
+                                   required>
+                        </div>
+                        <div class="col-md-6">
+                            <label for="lastname" class="form-label">Nama Belakang</label>
+                            <input class="form-control" name="lastname" id="lastname" placeholder="Nama Belakang *"
+                                   required>
+                        </div>
+                    </div>
+                    <div class="mt-3">
+                        <label for="email" class="form-label">Email</label>
+                        <input class="form-control" name="email" id="email" placeholder="Massukkan Alamat Email Anda *"
+                               required>
+                    </div>
+                    <div class="mt-3">
+                        <label for="password" class="form-label">Kata Sandi</label>
+                        <input type="password" class="form-control" name="password" id="password"
+                               placeholder="Massukkan Kata Sandi Anda *"
+                               required minlength="8" maxlength="20">
+                        <div id="passwordHelpBlock" class="form-text">
+                            Kata sandi Anda harus sepanjang 8-20 karakter.
+                        </div>
+                    </div>
+                    <div class="mt-3">
+                        <label for="date-of-birth" class="form-label">Tanggal Lahir</label>
+                        <input type="date" class="form-control" name="date-of-birth" id="date-of-birth"
+                               placeholder="Masukkan Tanggal Lahir Anda *" required>
+                    </div>
+                    <div class="mt-3">
+                        <label class="form-label">Jenis Kelamin</label>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="gender" value="Laki-laki" id="male"
+                                   checked>
+                            <label class="form-check-label" for="male">
+                                Laki-laki
+                            </label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="gender" value="Perempuan" id="female"
+                                   required>
+                            <label class="form-check-label" for="female">
+                                Perempuan
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" name="daftar" class="btn btn-dark">Daftar</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+        crossorigin="anonymous"></script>
+</body>
+</html>
