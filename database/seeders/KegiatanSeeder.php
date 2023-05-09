5ecd0eeccb7e848c96b82d109d820813ed94dd02
AlwanAlawi <alwanalawi1@gmail.com>
2023-05-09 09:58:52 +0700

    pengaduan seeder

--- database/seeders/KegiatanSeeder.php
+++ database/seeders/KegiatanSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class KegiatanSeeder extends Seeder
 {
@@ -14,6 +15,10 @@ class KegiatanSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('kegiatan')->insert([
+            [
+                'user_id' => ''
+            ]
+        ]);
     }
 }
--- database/seeders/PengaduanSeeder.php
+++ database/seeders/PengaduanSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class PengaduanSeeder extends Seeder
 {
@@ -14,6 +15,23 @@ class PengaduanSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('pengaduan')->insert([
+            [
+                'user_id' => '01',
+                'judul' => 'Air Keruh',
+                'isi' => 'Pada Siang hari saat saya mau memasak air tiba-tiba air yang akan kami gunakan menjadi keruh',
+                'tanggal_kejadian' => '2023-05-12',
+                'bukti_gambar' => '',
+                'is_read' => '0',
+            ],
+            [
+                'user_id' => '002',
+                'judul' => 'Air Mati',
+                'isi' => 'Pada Siang hari saat saya mau memasak air tiba-tiba air yang akan kami gunakan mati',
+                'tanggal_kejadian' => '2023-07-12',
+                'bukti_gambar' => '',
+                'is_read' => '1',
+            ]
+        ]);
     }
 }
