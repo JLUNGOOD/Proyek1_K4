31a3ba54043764069a264ce28ddb2ab5e8caf872
Sabilla Luthfi Rahmadhan <mamadhan017@gmail.com>
2023-05-14 10:32:31 +0700

    add footer

--- resources/views/user/layout.blade.php
+++ resources/views/user/layout.blade.php
@@ -67,6 +67,44 @@
 
 @yield('content')
 
+<footer class="bg-dark text-white pt-5 pb-4">
+    <div class="container text-center text-md-left">
+        <div class="row gy-3">
+            <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                <h5 class="text-uppercase mb-4 font-weight-bold text-warning">PDAM Kota Malang</h5>
+                <p>Sedikit informasi tentang kami. Terimakasih</p>
+            </div>
+            
+            <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Useful Links</h5>
+                <p><a href="#" class="text-white" style="text-decoration: none;">Pengaduan</a></p>
+                <p><a href="#" class="text-white" style="text-decoration: none;">Tanggapan</a></p>
+            </div>
+            
+            <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Contact Us</h5>
+                <p><i class="fas fa-home mr-3">Jl. Terusan Danau Sentani No.100, Madyopuro, Kec. Kedungkandang, Kota Malang</i></p>
+                <p><i class="fas fa-envelope mr-3">pdam@gmail.com</i></p>
+                <p><i class="fas fa-phone mr-3">0816550800</i></p>
+            </div>
+        </div>
+
+        <hr class="mb-4">
+
+        <div class="row align-items-center">
+
+            <div class="col-md-7 col-lg-8" style="margin-left: -100px">
+                <p>Copyright 2022. All rights reserved by:
+                    <a href="#" style="text-decoration: none;">
+                        <strong class="text-warning">Kelompok 4</strong>
+                    </a>
+                </p>
+            </div>
+
+        </div>
+    </div>
+</footer>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
         crossorigin="anonymous"></script>
