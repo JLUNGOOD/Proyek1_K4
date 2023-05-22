614d4f994e0a84071d7435c2b43bfc27abf0456f
Virgy Ferdian Surya Firmansyah <virgyferdian213@gmail.com>
2023-05-22 21:14:19 +0700

    fix bug always show signup modal

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -43,7 +43,8 @@
                 <button class="w-100 btn btn-lg btn-dark" type="submit">Sign In</button>
                 <hr class="my-4">
                 <small class="d-block text-muted text-center">Belum punya akun?
-                    <a data-bs-toggle="modal" data-bs-target="#daftar" id="btn-daftar" class="text-primary" style="cursor: pointer">
+                    <a data-bs-toggle="modal" data-bs-target="#daftar" id="btn-daftar" class="text-primary"
+                       style="cursor: pointer">
                         Daftar
                     </a>.
                 </small>
@@ -136,12 +137,18 @@
         </div>
     </div>
 </div>
+<script>
+    setTimeout(() => {
+        if (window.location.search.includes('signup')) {
+            document.querySelector('#btn-daftar').click();
 
-@if(request()->has('signup'))
-    <script>
-        setTimeout(() => document.querySelector('#btn-daftar').click(), 500);
-    </script>
-@endif
+            // Remove the 'signup' parameter from the URL
+            const newUrl = window.location.href.replace('?signup=true', '');
+            window.history.replaceState({}, document.title, newUrl);
+        }
+        ;
+    }, 500);
+</script>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
