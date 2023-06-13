18ec430aa4d6db536e0982715f34e3218cc7423e
Bahtiar Rifai <bahtiarderifai@gmail.com>
2023-06-13 10:56:41 +0700

    add modal info status 'pengaduan'

--- resources/views/admin/tanggapi_laporan.blade.php
+++ resources/views/admin/tanggapi_laporan.blade.php
@@ -29,6 +29,9 @@
                                 <option value="rejected" onclick="getRejected()">Rejected</option>
                             </select>
                         </div>
+                        <button class="btn btn btn-outline-dark" data-bs-toggle="modal" data-bs-target="#modalInfo">
+                            <i class="fas fa-question"></i>
+                        </button>
                     </div>
                 </div>
                 <div class="col-6 col-sm-3">
@@ -89,11 +92,29 @@
                 </div>
             @endforeach
         </div>
-        <p>*Notes :</p>
-        <ul>
-            <li>Centang Hitam = belum dibaca oleh petugas/admin</li>
-            <li>Centang Biru = sudah dibaca oleh petugas/admin</li>
-        </ul>
+
+        <div class="modal fade" id="modalInfo" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+             aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="staticBackdropLabel">Informasi Status Pengaduan</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <p>
+                            <i class='fas fa-check-double pe-2'></i>
+                            Belum Dibaca oleh Petugas/Admin.
+                        </p>
+                        <hr>
+                        <p>
+                            <i class='fas fa-check-double text-info pe-2'></i>
+                            Sudah Dibaca oleh Petugas/Admin.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
 
@@ -129,7 +150,7 @@
                                     <a class="btn btn-dark" href="/admin/tanggapi/${pengaduan['id']}">Lihat
                                         Rincian</a>
                                 </div>
-                                <div class="card-footer text-muted d-flex justify-content-between">
+                                <div class="card-footer text-muted d-flex justify-content-between align-items-center">
                                     ${pengaduan['tanggal_kejadian']}
                                     ${pengaduan['status'] == "1" ? "<span class='badge bg-success'>Solved</span>" :
                                         pengaduan['status'] == "0" ? "<span class='badge bg-danger'>Unsolved</span>" :
