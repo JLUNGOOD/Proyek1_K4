5f5e9bf99a3d1219f58805b0d5cbeced4426abae
Sabilla Luthfi Rahmadhan <mamadhan017@gmail.com>
2023-06-24 20:07:50 +0700

    Add legend dashboard chart

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -63,26 +63,61 @@
 @push('script')
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script>
-        const ctx = document.getElementById('myChart');
-        new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: {!! $categories_name !!},
-                datasets: [{
-                    label: 'Banyak Pengaduan',
-                    data: {{ $pengaduan_count }},
-                    backgroundColor: 'rgba(10, 125, 255, 1)',
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                },
-                aspectRatio: 5/1,
-            },
-        });
-    </script>
+    const ctx = document.getElementById('myChart');
+    new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: {!! $categories_name !!},
+        datasets: [{
+        label: 'Banyak Pengaduan',
+        data: {{ $pengaduan_count }},
+        backgroundColor: [
+            'rgba(10, 125, 255, 1)', 
+            'rgba(255, 99, 132, 1)', 
+            'rgba(54, 162, 235, 1)', 
+            'rgba(255, 206, 86, 1)', 
+            'rgba(75, 192, 192, 1)', 
+            'rgba(153, 102, 255, 1)', 
+            'rgba(255, 159, 64, 1)' 
+        ],
+        borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+        y: {
+            beginAtZero: true
+        }
+        },
+        aspectRatio: 5 / 1,
+        plugins: {
+        legend: {
+            display: true,
+            position: 'top',
+            labels: {
+            generateLabels: function (chart) {
+                const data = chart.data;
+                if (data.labels.length && data.datasets.length) {
+                return data.labels.map(function (label, i) {
+                    const meta = chart.getDatasetMeta(0);
+                    const style = meta.controller.getStyle(i);
+
+                    return {
+                    text: label + ': ' + data.datasets[0].data[i], 
+                    fillStyle: style.backgroundColor, 
+                    strokeStyle: style.borderColor,
+                    lineWidth: style.borderWidth,
+                    hidden: isNaN(data.datasets[0].data[i]) || meta.data[i].hidden,
+                    index: i
+                    };
+                });
+                }
+                return [];
+            }
+            }
+        }
+        }
+    }
+    });
+</script>
 @endpush
